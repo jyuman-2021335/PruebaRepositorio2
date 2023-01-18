242c307877737fd44f7c6e1193097d128e7df110
Jefferson <jyuman-2021335@kinal.edu.gt>
2023-01-18 14:53:08 -0600

    Primer Commit en terminal de VS Code

---
+++ index.js
@@ -0,0 +1,8 @@
+function getSuma(){
+    const valor1 = Number(document.getElementById('valor1').value);
+    const valor2 = Number(document.getElementById('valor2').value);
+    
+    const suma = valor1 + valor2;
+
+    console.log(suma);
+}
